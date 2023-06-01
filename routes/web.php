cee229bdcc1d22e2f307e74628322279fb52afd0
Havvazeynep <havva.zeynep.16@gmail.com>
2023-06-01 10:36:10 +0300

    https://www.youtube.com/watch?v=ER-wOKXrbuw videosu ile basit bir chat uygulaması yapıldı.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PusherController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::controller(PusherController::class)->group(function() {
+    Route::get('/', 'index');
+    Route::post('/broadcast', 'broadcast');
+    Route::post('/receive', 'receive');
 });
